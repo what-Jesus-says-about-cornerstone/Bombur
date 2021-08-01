54c3f0419e2a4655476811e5a6fe7103cdda4fe0
kafka-scout <kafka-scout@program>
2021-08-01 03:57:59 +0300

    in program.sh i remove no history push

--- program.sh
+++ program.sh
@@ -4,15 +4,4 @@ repl(){
   clj -M:repl
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am kafka-scout program"
-  git push -f -u origin main
-}
-
 "$@"
