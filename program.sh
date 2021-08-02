fc7586f64ffc11382112b0b3d7807cff77110405
kafka-scout <kafka-scout@program>
2021-08-02 23:00:25 +0300

    i use new commit hash of deps-repl - with clj -X

--- program.sh
+++ program.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 repl(){
-  clj -M:repl
+  clj \
+    -X:repl deps-repl.core/process \
+    :main-ns deathstar.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
 }
 
 "$@"
