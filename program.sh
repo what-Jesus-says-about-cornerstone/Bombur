3898d80818b8ad2ef59295704f14eb86601236d8
kafka-scout <kafka-scout@program>
2021-08-03 21:13:34 +0300

    i revert "i remove :main alias and move code to program.sh - reps.edn is for libraries only"
    This reverts commit 5260dfce3b0c95f3984014a63352b3f7522f2628.

--- program.sh
+++ program.sh
@@ -10,11 +10,4 @@ repl(){
     :nrepl? false
 }
 
-main(){
-  clojure \
-    -J-Dclojure.core.async.pool-size=1 \
-    -J-Dclojure.compiler.direct-linking=false \
-    -M -m kafka-scout.main
-}
-
 "$@"
